7be6201f9450b9f907c975887420fa423e927ccb
Nana Osei <osei.somuah2015@gmail.com>
2020-09-21 17:35:55 +0000

    last update

--- validation.js
+++ validation.js
@@ -1,121 +1,39 @@
-const myform = document.getElementById("myform")
+
+const myform = document.getElementById("myform");
 const fname = document.getElementById("fname");
 const lname = document.getElementById("lname");
 const phone = document.getElementById("phone");
 const email = document.getElementById("email");
 
 
-// function errorChecker(){
-//     if(fnamevalid() && lnamevalid() && phonevalid() && emailvalid()){
-//         return true;
-//     }
-//     else{
-//         return false;
-//     }
-// }
-
-function fnamevalid(){
-    if(!alphaOnly(fname)){
-        return true;
-    }
-    else{
+function errorChecker(){
+    const fname = document.getElementById("fname").value;
+    if(/^[a-zA-Z ]+$/.test(fname) === false){
+        alert("'First name' allows Only alphabets!");
         return false;
     }
-}
 
-function lnamevalid(){
-    if(!alphaOnly(lname)){
-        return true;
-    }
-    else{
+    const lname = document.getElementById("lname").value;
+    if(/^[a-zA-Z ]+$/.test(lname) === false){
+        alert("'Last name' allows Only alphabets!");
         return false;
-    }
-}
+    }    
 
-function phonevalid(){
-    if(!phoneOnly(phone)){
-        return true;
-    }
-    else{
+    const phone = document.getElementById("phone").value;
+    if(/^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s\./0-9]*$/.test(phone) === false){
+        alert("'Phone Number' allows Only numbers!");
         return false;
-    }
-}
+    } 
 
-function emailvalid(){
-    if(!emailOnly(email)){
-        return true;
-    }
-    else{
+    const email = document.getElementById("email").value;
+    if(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email) === false){
+        alert("Please use the correct email format!");
         return false;
-    }
+    } 
+    return errorChecker();
 }
 
 
-
-function noInput(input){
-    if(empty(input.value)){
-        errorNote(input,"Please fill here")
-        return true;
-    }
-    else{
-        goodInput(input);
-        return false;
-    }
-}
-
-function empty(data){
-    if(data===""){
-        return true;
-    }
-    else{
-        return false;
-    }
-}
-
-function errorNote(input, note){
-    input.className = "error";
-    input.nextElementSibling.innerHTML = note;
-    input.nextElementSibling.color = red;
-}
-
-function goodInput(input){
-    input.className = "good";
-    input.nextElementSibling.innerHTML ="";
-    input.nextElementSibling.color = green;
-}
-
-function alphaOnly(input){
-    if(/^[a-zA-Z ]+$/.test(input.value)){
-         goodInput(input);
-         return true;
-    }
-    else{
-        errorNote(input,"Only alphabets allowed here!");
-        return false;
-    }
-}
-
-function emailOnly(input){
-    if(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(input.value)){
-        goodInput(input);
-        return true;
-    }
-    else{
-        errorNote(input,"Please use the correct email format!");
-        return false;
-    }
-}
-
-function phoneOnly(input){
-    if(/^[0-9 ]+$/.test(input.value)){
-         goodInput(input);
-         return true;
-    }
-    else{
-        errorNote(input,"Only numbers allowed here!");
-        return false;
-    }
-}
 
 function dataTrans(){
    const fname1 = document.getElementById("fname").value;
@@ -151,3 +69,5 @@ window.addEventListener("load", () =>{
     document.getElementById("remail").innerHTML = email1;
 
 })
+
+
